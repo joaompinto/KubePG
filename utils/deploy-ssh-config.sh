65ae3c4d3b8e2ab0d07cc2848532cb0d217ea5ff
João Pinto <jpinto@ccsint.eu>
2019-08-06 21:33:57 +0100

    Added the deploy ssh config

---
+++ utils/deploy-ssh-config.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+export LIBVIRT_DEFAULT_URI=qemu:///system
+
+kubepg_net=$(virsh net-dumpxml kubepg | grep -oP "ip address='\K\d+\.\d+.\d+")
+
+[ ! -d ~/.ssh ] && mkdir -m700 ~/.ssh
+
+for i in $(seq 1 6)
+do
+    host="kpg-node${i}"
+    sed -i 's/^Host/\n&/' ~/.ssh/config
+    sed -i '/^Host '"$host"'$/,/^$/d;/^$/d' ~/.ssh/config
+    cat << _EOF_ >> ~/.ssh/config
+Host ${host}
+    Hostname ${kubepg_net}.1${i}
+    User root
+    IdentityFile ~/.kubepg/kubepg_id
+
+_EOF_
+done
+chmod 700 ~/.ssh/config
+
+
